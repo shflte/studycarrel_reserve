7cc33f9ecea0559bf50a9f10bb039c9c638f9b44
YC CHEN <shflte1314527@gmail.com>
2023-10-05 17:10:31 +0800

    rename

--- captcha_hack.py
+++ captcha_hack.py
@@ -1,103 +1,99 @@
-from dotenv import load_dotenv
-import subprocess
-import requests
+import csv
+import cv2
+import numpy as np
+import random
 import os
-import time
-import base64
 
-def get_captcha_value():
-    load_dotenv()
+from tqdm import tqdm
 
-    api_key = os.getenv("2CAPTCHA_API_KEY")
-    url = "https://api.2captcha.com/"
-    create_task_endpoint = url + "createTask"
-    get_task_result_endpoint = url + "getTaskResult"
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision.models import resnet50
+import torchvision
+from torchvision import transforms
+import json
+from PIL import Image
+import re
+import heapq
+import joblib
 
-    create_payload = {
-        "clientKey": api_key,
-        "task": {
-            "type": "ImageToTextTask",
-            "body": "",
-            "phrase": False,
-            "case": True,
-            "numeric": 1,
-            "math": False,
-            "minLength": 4,
-            "maxLength": 4,
-        },
-        "languagePool": "en"
-    }
-    get_payload = {
-    "clientKey": api_key, 
-    "taskId": 0
-    }
+def ImageToString(image_name):
+    TEST_PATH = "."
+    device = "cpu"
+    IMAGE_NAME = image_name
 
-    '''
-    response example
-    createTask:
-    {
-        "errorId": 0,
-        "taskId": 72345678901
-    }
-    getTaskResult:
-    {
-        "errorId": 0,
-        "status": "ready",
-        "solution": {
-            "text": "hello world"
-        },
-        "cost": "0.00025",
-        "ip": "1.2.3.4",
-        "createTime": 1692808229,
-        "endTime": 1692808326,
-        "solveCount": 1
-    }
-    '''
+    class Task3Dataset(Dataset):
+        def __init__(self, data, root, return_filename=False):
+            self.data = [sample for sample in data]
+            self.return_filename = return_filename
+            self.root = root
+        
+        def __getitem__(self, index):
+            filename, label = self.data[index]
 
-    headers = {
-        "Content-Type": "application/json"
-    }
+            target_size = (125, 68)
+            resize_transform = transforms.Resize(target_size)
+            img = torchvision.io.read_image(f"{self.root}/{filename}")
+            img = resize_transform(img)
+            img_pil = transforms.ToPILImage()(img)
 
-    # load captcha image and encode it into base64
-    captcha_path = "captcha.png"
-    with open(captcha_path, "rb") as f:
-        captcha_image = f.read()
-        captcha_image_encoded = base64.b64encode(captcha_image).decode("utf-8")
-        create_payload["task"]["body"] = captcha_image_encoded
-    create_response = requests.post(create_task_endpoint, json=create_payload, headers=headers)
+            
+            transform = transforms.Compose([
+                transforms.ToPILImage(), 
+                transforms.Resize(288), 
+    #             transforms.CenterCrop(224),
+                # transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # 標準化
+            ])
+            img = transform(img)
 
-    # check if create task is successful
-    if create_response.status_code != 200 :
-        raise Exception(f"create task failed: {create_response.json()}")
-    create_response_json = create_response.json()
-    if create_response_json["errorId"] != 0:
-        raise Exception(f"create task failed: {create_response_json}")
+            if self.return_filename:
+                return torch.FloatTensor(img / 255), filename
+            else:
 
-    # get task id
-    task_id = create_response_json["taskId"]
+                table = ['0','1','2','3','4','5','6','7','8','9']
+                one_hot = [0]*40
+                index0 = table.index(label[0])
+                index1 = table.index(label[1])+10
+                index2 = table.index(label[2])+20
+                index3 = table.index(label[3])+30
+                one_hot[index0] = 1
+                one_hot[index1] = 1
+                one_hot[index2] = 1
+                one_hot[index3] = 1
+                return torch.FloatTensor(img / 255), torch.LongTensor(one_hot)
 
-    # get task result
-    get_payload["taskId"] = task_id
-    while True:
-        get_response = requests.post(get_task_result_endpoint, json=get_payload, headers=headers)
-        # check if get task result is successful
-        if get_response.status_code != 200 :
-            raise Exception(f"get task result failed: {get_response.json()}")
-        get_response_json = get_response.json()
-        print(get_response_json)
+        def __len__(self):
+            return len(self.data)
 
-        # check if get task result is successful
-        if get_response_json["errorId"] != 0:
-            raise Exception(f"get task result failed: {get_response_json}")
+    test_data = [[IMAGE_NAME, '0']]
+    test_ds3 = Task3Dataset(test_data, root=TEST_PATH, return_filename=True)
+    test_dl3 = DataLoader(test_ds3, batch_size=32, num_workers=4, drop_last=False, shuffle=False)
 
-        # check if task is ready
-        if get_response_json["status"] == "ready":
-            break
-        else:
-            print("task not ready, wait for 1 second")
-            time.sleep(1)
+    model3 = torchvision.models.resnet18(pretrained=True).to(device)
+    model3.fc = nn.Linear(in_features=512, out_features=40, bias=True).to(device)
+    model3.load_state_dict(torch.load("./captcha_model", map_location=torch.device('cpu')))
+    model3.eval()
 
-    # get captcha value
-    captcha_value = get_response_json["solution"]["text"]
+    table = ['0','1','2','3','4','5','6','7','8','9']
+    for image, filenames in test_dl3:
+        image = image.to(device)
+        pred = model3(image)
+        for i in range(len(filenames)):
+            ans = ""
+            large1_pred = torch.argmax(pred[i][0:10])
+            large2_pred = torch.argmax(pred[i][10:20])
+            large3_pred = torch.argmax(pred[i][20:30])
+            large4_pred = torch.argmax(pred[i][30:40])
+            ans = ans + table[large1_pred]
+            ans = ans + table[large2_pred]
+            ans = ans + table[large3_pred]
+            ans = ans + table[large4_pred]
+            return ans
+        del image, filenames
 
-    return captcha_value
+
+ans = ImageToString('14.png')
+print(ans)
